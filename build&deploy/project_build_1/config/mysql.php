bdf5353c4c8fc7479750b4073961159fbb8cb2e9
Andrey Eroshenkov <andrey.smolensky67@gmail.com>
2020-05-12 09:49:39 +0400

    Change folders name

---
+++ build&deploy/project_build_1/config/mysql.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => yii\db\Connection::class,
+    'dsn' => 'mysql:host=mysql:3306;dbname=mysql_db',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
